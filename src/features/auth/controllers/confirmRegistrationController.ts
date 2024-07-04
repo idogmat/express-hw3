2b5c1e1019cc603da2e3d0f00b62b9aa74948808
idogmat <idogmat@gmail.com>
2024-07-04 20:53:37 +0300

    add: auth

--- src/features/auth/controllers/confirmRegistrationController.ts
+++ src/features/auth/controllers/confirmRegistrationController.ts
@@ -7,8 +7,12 @@ export interface ICode {
 export const confirmRegistrationController = async (req: Request<{}, {}, {}, ICode>, res: Response<any>) => {
   const user = await userCollection.findOne({'emailConfirmation.confirmationCode': req.query.code})
   console.log(user)
-  if (user) {
-    const updated = await userCollection.findOneAndUpdate({'emailConfirmation.confirmationCode': req.query.code}, {$set:{'emailConfirmation.isConfirmed': true}}, { returnDocument: 'after'})
+  if (!user?.emailConfirmation?.isConfirmed) {
+    const updated = await userCollection.findOneAndUpdate(
+      {'emailConfirmation.confirmationCode': req.query.code},
+      {$set:{'emailConfirmation.isConfirmed': true}},
+      { returnDocument: 'after'});
+
     if (updated?.emailConfirmation.isConfirmed) {
       return res.sendStatus(204)
     } else {
--- src/features/auth/middlewares/validators.ts
+++ src/features/auth/middlewares/validators.ts
@@ -2,6 +2,7 @@ import { body, param, query } from 'express-validator'
 import { inputCheckErrorsMiddleware } from '../../../global-middlewares/inputCheckErrorsMiddleware'
 import { blogCollection } from '../../../app'
 import { ObjectId } from 'mongodb'
+import { authRepository } from '../authRepository'
 
 
 export const codeParamsValidator = query('code').isString().trim().withMessage('not string').isLength({ min: 1, max: 500 }).withMessage('not found')
@@ -12,19 +13,27 @@ export const emailConfirmCreateValidators = [
 ]
 
 export const loginValidator = body('login').isString().withMessage('not string')
-  .trim().isLength({ min: 3, max: 10 }).withMessage('more then 30 or 0').custom(login =>{
-    const regex = /^[a-zA-Z0-9_-]*$/;
-    if (!regex.test(login)) {
-      return Promise.reject('not valid')
+  .trim().isLength({ min: 3, max: 10 }).withMessage('more then 30 or 0').custom(async login =>{
+    const user = await authRepository.findByLoginOrEmail(login);
+    if(!user)  {
+      return Promise.resolve()
+    }else {
+      return Promise.reject()
     }
-    return true
-  })
+  })  
 
 export const passwordValidator = body('password').isString().withMessage('not string')
   .trim().isLength({ min: 6, max: 20 }).withMessage('more then 100 or 0')
 
 export const emailValidator = body('email').isString().withMessage('not string')
-  .trim().isEmail().withMessage('not valid email')
+  .trim().isEmail().withMessage('not valid email').custom(async email =>{
+    const user = await authRepository.findByLoginOrEmail(email);
+    if(!user)  {
+      return Promise.resolve()
+    }else {
+      return Promise.reject()
+    }
+  })
 
 export const userCreateValidators = [
   loginValidator,
